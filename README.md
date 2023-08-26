52a0b503f968a04763e7f50e00742d6088508043
diyooor <diyorsattarov7@gmail.com>
2023-08-26 17:32:02 -0500

    Removed client id/secret from test/test_main.cpp -- updated README with Testing && Getting started

--- README.md
+++ README.md
@@ -16,8 +16,36 @@ Before you begin, ensure you have met the following requirements:
 
 - CMake (minimum version 3.2)
 - C++ compiler (supporting C++11 or later)
-
+- Boost/Beast
+- spdlog
+- googletest
 ## Getting Started
+```
+git clone https://github.com/diyooor/cpp-chatbot
+cmake .
+cmake --build .
+./test/bin/test_main.exe
+```
+## Testing
+```
+$ ./test/bin/test_main.exe
+[==========] Running 1 test from 1 test suite.
+[----------] Global test environment set-up.
+[----------] 1 test from MyTestFixture
+[ RUN      ] MyTestFixture.TestHttpClient
+[2023-08-26 17:16:46.897] [info] Welcome to spdlog!
+[2023-08-26 17:16:46.898] [info] Start of test fixture!
+[2023-08-26 17:16:47.401] [info] Response status code: 200
+[2023-08-26 17:16:47.402] [info] Response body: {"access_token":".....","expires_in":5453292,"token_type":"bearer"}
+
+[       OK ] MyTestFixture.TestHttpClient (728 ms)
+[----------] 1 test from MyTestFixture (735 ms total)
+
+[----------] Global test environment tear-down
+[==========] 1 test from 1 test suite ran. (753 ms total)
+[  PASSED  ] 1 test.
+
+```
 
 ## Contributing
 
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -2,62 +2,60 @@
 #include <string>
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/ssl.hpp>
 #include <gtest/gtest.h>
 #include <spdlog/spdlog.h>
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace asio = boost::asio;
+namespace ssl = boost::asio::ssl;
+using tcp = boost::asio::ip::tcp;
 
 // Base fixture with common setup and teardown
 class BaseTestFixture : public ::testing::Test {
 protected:
-    BaseTestFixture() : io_context(), ssl_context(boost::asio::ssl::context::sslv23) {
+    BaseTestFixture() : io_context(), ssl_context(ssl::context::sslv23) {
         ssl_context.set_default_verify_paths();
         spdlog::info("Welcome to spdlog!");
-        // Create a console logger for the base fixture
-        //console_logger = spdlog::stdout_logger_mt("console");
-        //console_logger->set_pattern("[%H:%M:%S %z] [%l] %v");
     }
 
-    boost::asio::io_context io_context;
-    boost::asio::ssl::context ssl_context;
-    //std::shared_ptr<spdlog::logger> console_logger; // Console logger instance
+    asio::io_context io_context;
+    ssl::context ssl_context;
 };
-
 
 // Derived fixture for specific test cases
 class MyTestFixture : public BaseTestFixture {
 };
 
 TEST_F(MyTestFixture, TestHttpClient) {
-    boost::asio::ssl::stream<boost::asio::ip::tcp::socket> socket(io_context, ssl_context);
+    spdlog::info("Start of test fixture!");
+    ssl::stream<tcp::socket> socket(io_context, ssl_context);
+    tcp::resolver resolver(io_context);
+    tcp::resolver::results_type endpoints = resolver.resolve("id.twitch.tv", "https");
 
-    boost::asio::ip::tcp::resolver resolver(io_context);
-    boost::asio::connect(socket.lowest_layer(), resolver.resolve("id.twitch.tv", "https"));
+    boost::asio::connect(socket.next_layer(), endpoints.begin(), endpoints.end());
 
-    socket.handshake(boost::asio::ssl::stream_base::client);
+    socket.handshake(ssl::stream_base::client);
 
-    std::string client_id = "your_client_id";
-    std::string client_secret = "your_client_secret";
+    http::request<http::string_body> req{http::verb::post, "/oauth2/token", 11};
+    req.set(http::field::host, "id.twitch.tv");
+    req.set(http::field::content_type, "application/x-www-form-urlencoded");
+    std::string client_id = "client_id";
+    std::string client_secret = "client_secret";
+    req.body() = "client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials";
+    req.prepare_payload();
 
-    std::string request =
-        "POST /oauth2/token HTTP/1.1\r\n"
-        "Host: id.twitch.tv\r\n"
-        "Content-Type: application/x-www-form-urlencoded\r\n"
-        "Content-Length: " + std::to_string(91 + client_id.length() + client_secret.length()) + "\r\n"
-        "\r\n" +
-        "client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials";
+    http::write(socket, req);
 
-    boost::asio::write(socket, boost::asio::buffer(request));
+    beast::flat_buffer buffer;
+    http::response<http::dynamic_body> res;
+    http::read(socket, buffer, res);
 
-    boost::asio::streambuf response;
-    boost::asio::read_until(socket, response, "\r\n");
-
-    std::ostringstream response_stream;
-    response_stream << &response;
-    std::cout << response_stream.str();
-
-    socket.shutdown();
-    socket.lowest_layer().close();
-
-    EXPECT_FALSE(response_stream.str().empty());
+    spdlog::info("Response status code: {}", res.result_int());
+    spdlog::info("Response body: {}", beast::buffers_to_string(res.body().data()));
+    EXPECT_EQ(res.result(), http::status::ok);
 }
 
 int main(int argc, char** argv) {
